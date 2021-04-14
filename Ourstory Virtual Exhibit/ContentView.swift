b06006261f7ea881b7ede6f758365a5f5f827f12
Yeseo Kim <YKim@lcs.on.ca>
2021-04-13 20:40:50 -0400

    Initial Commit

---
+++ Ourstory Virtual Exhibit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Ourstory Virtual Exhibit
+//
+//  Created by Yeseo Kim on 2021-04-13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Ourstory Virtual Exhibit/Ourstory_Virtual_ExhibitApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ourstory_Virtual_ExhibitApp.swift
+//  Ourstory Virtual Exhibit
+//
+//  Created by Yeseo Kim on 2021-04-13.
+//
+
+import SwiftUI
+
+@main
+struct Ourstory_Virtual_ExhibitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
